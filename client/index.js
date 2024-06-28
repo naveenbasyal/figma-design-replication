286118c3559333f1e7b48c2935da9df91cbd413e
Naveen Basyal <naveenbasyal.001@gmail.com>
2024-06-28 10:23:34 +0530

    gathering form data

--- client/index.js
+++ client/index.js
@@ -8,3 +8,16 @@ document.querySelector('.overlay-close-btn').addEventListener('click', () => {
 function closeOverlay() {
     document.getElementById('overlay').style.display = 'none';
 }
+/* gather all the values of form fields and console log them like {name:"naveen",email:"naveen@gmail.com",phone:628042379,stage:0-6} */
+
+document.querySelector('.overlay__form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log("document-->", document.getElementById('stage'));
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('countrycode').value + document.getElementById('phone').value;
+    const stage = document.getElementById('stage').value;
+    console.log({ name, email, phone, stage });
+    // closeOverlay();
+}
+)
