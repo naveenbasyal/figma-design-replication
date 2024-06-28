4264d9f4d043b2fa54367392b256514bda40d85b
Naveen Basyal <naveenbasyal.001@gmail.com>
2024-06-28 17:17:31 +0530

    implemented user registeration

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 document.querySelector('.navbar__booking-link').addEventListener('click', () => {
-    console.log('clicked');
+
     document.getElementById('overlay').style.display = 'flex';
 })
 document.querySelector('.overlay-close-btn').addEventListener('click', () => {
@@ -8,16 +8,41 @@ document.querySelector('.overlay-close-btn').addEventListener('click', () => {
 function closeOverlay() {
     document.getElementById('overlay').style.display = 'none';
 }
-/* gather all the values of form fields and console log them like {name:"naveen",email:"naveen@gmail.com",phone:628042379,stage:0-6} */
 
-document.querySelector('.overlay__form').addEventListener('submit', (e) => {
+
+document.querySelector('.overlay__form').addEventListener('submit', async (e) => {
     e.preventDefault();
     console.log("document-->", document.getElementById('stage'));
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const phone = document.getElementById('countrycode').value + document.getElementById('phone').value;
     const stage = document.getElementById('stage').value;
-    console.log({ name, email, phone, stage });
-    // closeOverlay();
+    const applyNowBtn = document.querySelector('.apply-now-btn');
+    applyNowBtn.textContent = 'Loading...';
+    applyNowBtn.disabled = true;
+    try {
+        const response = await fetch('http://localhost:8000/api/user/apply', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, phone, stage })
+        })
+        const data = await response.json();
+        console.log("data", data)
+        alert(data.message)
+        return data;
+
+    } catch (error) {
+        console.log("error aya", err);
+        alert("error occured", err.message)
+
+    }
+    finally {
+        applyNowBtn.textContent = 'Apply Now';
+        applyNowBtn.disabled = false;
+        closeOverlay();
+    }
+
 }
 )
